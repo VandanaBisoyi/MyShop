61494e06ef94c3f18cd822da7bb8625ccf92d8a5
VandanaBisoyi <149338273+VandanaBisoyi@users.noreply.github.com>
2023-12-28 15:53:18 -0800

    Getting started with Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import {Container} from 'react-bootstrap'
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import HomeScreen from './screens/HomeScreen';
+import Homescreen from './screens/Homescreen';
 import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
