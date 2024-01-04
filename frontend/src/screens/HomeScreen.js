484124f226772c4a32305b90516323fd9c065bc2
VandanaBisoyi <149338273+VandanaBisoyi@users.noreply.github.com>
2024-01-04 14:57:17 -0800

    integrate frontend with backend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+import Product from '../components/Product'
+import { Col, Row } from 'react-bootstrap'
 
-const Homescreen = () => {
+const HomeScreen = () => {
+
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const {data} = await axios.get('/api/products')
+      setProducts(data)
+    }
+
+    fetchProducts()
+  })
+
   return (
-    <div>Homescreen</div>
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map(p => (
+          <Col sm={12} md={6} lg={4} xl={3}>
+            <Product product={p} />
+          </Col>
+          ))
+        }
+      </Row>
+    </>
   )
 }
 
-export default Homescreen
+export default HomeScreen
