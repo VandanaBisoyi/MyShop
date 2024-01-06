d9c1f867cccba78e78ed810eae3c6ce36f281919
VandanaBisoyi <149338273+VandanaBisoyi@users.noreply.github.com>
2024-01-06 09:07:07 -0800

    BE and FE integration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import {Container} from 'react-bootstrap'
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Homescreen from './screens/Homescreen';
+import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
