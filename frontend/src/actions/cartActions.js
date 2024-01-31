3c07e367cd3f326f725097fdbc1a0c73a5281f90
VandanaBisoyi <149338273+VandanaBisoyi@users.noreply.github.com>
2024-01-31 13:52:00 -0800

    Completed cart functionality and added controllers

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,15 +1,27 @@
-import {configureStore, combineReducers} from '@reduxjs/toolkit'
-import { productListReducer, productDetailsReducer } from './reducers/productReducers'
-import { cartReducer } from './reducers/cartReducers'
+import axios from 'axios'
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants'
 
-const rootReducer = combineReducers({
-  productList: productListReducer,
-  productDetails: productDetailsReducer,
-  cart: cartReducer
-})
-const store = configureStore({
-  reducer: rootReducer,
-  preloadedState: {}
-})
+export const addToCart = (id, qty) => async (dispatch, getState) => {
+  const {data} = await axios.get(`/api/products/${id}`)
+  dispatch({
+    type: CART_ADD_ITEM,
+    payload: {
+      product: data._id,
+      name: data.name,
+      image: data.image,
+      price: data.price,
+      countInStock: data.countInStock,
+      qty
+    }
+  })
 
-export default store
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems) )
+}
+
+export const removeFromCart = (id) => (dispatch, getState) => {
+  dispatch({
+    type: CART_REMOVE_ITEM,
+    payload: id
+  })
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems) )
+}
